71e685bd27ef7540e184554a95fe97b2e4929ec5
Edmundo rubio <edmundo@webaholics.co>
2019-02-18 22:31:03 -0700

    update code

--- src/AppCode/Pages/Main.jsx
+++ src/AppCode/Pages/Main.jsx
@@ -6,6 +6,7 @@ class Main extends React.Component{
       <div>
         <h1>App C.D.</h1>
         <p>This is the text on the application...</p>
+        <p>Second Paragraph...</p>
       </div>
     )
   }
